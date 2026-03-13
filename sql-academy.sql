f4b14816452904be79eea7268f17fd6d743971cd
xenia-kolchina <kyukolchina@gmail.com>
2026-03-13 11:06:06 +0300

    Update sql-academy.sql

--- sql-academy.sql
+++ sql-academy.sql
@@ -26,25 +26,44 @@ SELECT COUNT(*) as count
 FROM Trip
 WHERE Trip.plane = 'TU-134'
 
--- ДЕНЬ 2 (13.03.2026). Темы 1-6 по SQL от Changellenge, решение задач 6-10 с SQL Academy.
+-- ДЕНЬ 2 (13.03.2026). Тема 7 по SQL от Changellenge, решение задач 6-10 с SQL Academy.
 -- Задача 6. Компании, летавшие на Boeing
 -- Какие компании совершали перелеты на Boeing
-
+SELECT DISTINCT name
+FROM Trip
+LEFT JOIN 
+Company
+ON Trip.company = Company.id
+AND Trip.plane = 'Boeing'
+WHERE name is NOT NULL 
 
 -- Задача 7. Самолеты, летящие в Москву
 -- Вывести все названия самолётов, на которых можно улететь в Москву (Moscow)
-
+select DISTINCT Trip.plane
+from Trip
+where Trip.town_to = 'Moscow'
 
 -- Задача 8. Полёты из Парижа
 -- В какие города можно улететь из Парижа (Paris) и сколько времени это займёт?
 -- Используйте конструкцию "as flight_time" для вывода необходимого времени. Это необходимо для корректной проверки.
 -- Формат для вывода времени: HH:MM:SS
-
+select DISTINCT Trip.town_to,
+TIMEDIFF(Trip.time_in, Trip.time_out) as flight_time
+from Trip
+where Trip.town_from = 'Paris'
 
 -- Задача 9. Компании с рейсами из Владивостока
 -- Какие компании организуют перелеты из Владивостока (Vladivostok)? 
-
+select name
+from Trip
+left JOIN 
+Company
+on Trip.company = Company.id
+where town_from = 'Vladivostok'
 
 -- Задача 10. Вылеты в определенное время
 -- Вывести вылеты, совершенные с 10 ч. по 14 ч. 1 января 1900 г.
+select *
+from Trip
+where Trip.time_out BETWEEN '1900-01-01T10:00:00.000Z' and '1900-01-01T14:00:00.000Z'
 
